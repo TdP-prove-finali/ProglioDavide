409502a0dc79aeed8999f88ff5bee8296386d559
Proglio Davide <s272199@studenti.polito.it>
2023-05-07 17:32:26 +0200

    Versione aggiornata

--- src/main/java/it/polito/tdp/tesi/model/Model.java
+++ src/main/java/it/polito/tdp/tesi/model/Model.java
@@ -8,14 +8,11 @@ import it.polito.tdp.tesi.db.ordiniDAO;
 public class Model {
 	
 	private ordiniDAO ordiniDAO;
-	//private LotSizing2 lotSizing;
 	private LotSizing3 lS;
-	
 	private Simulazione3 sim;
 	
 	public Model() {
 		this.ordiniDAO = new ordiniDAO();
-		//this.lotSizing = new LotSizing2();
 		this.lS = new LotSizing3();
 		this.sim = new Simulazione3();
 	}
@@ -27,20 +24,6 @@ public class Model {
 	public ArrayList<String> getProdByYear(int anno){
 		return this.ordiniDAO.getProdByYear(anno);
 	}
-	
-	/*public ArrayList<Domanda> getDomandaMese(String idProdotto, int anno){
-		ArrayList<Domanda> domanda = new ArrayList<Domanda>();
-		HashMap<Integer, Integer> qta = new HashMap<Integer, Integer>();
-		qta = this.ordiniDAO.getQtaProdottoMese(idProdotto, anno);
-		for(int mese : qta.keySet()) {
-			domanda.add(new Domanda(mese, qta.get(mese)));
-		}
-		return domanda;
-	}
-	
-	public HashMap<Integer,Integer> getDomandaMese2(String idProdotto, int anno){
-		return this.ordiniDAO.getQtaProdottoMese(idProdotto, anno);
-	}*/
 	
 	public ArrayList<Domanda> getDomandaMensile(String idProdotto, int anno){
 		return this.ordiniDAO.getDomandaMese(idProdotto, anno);
@@ -77,7 +60,6 @@ public class Model {
 	}
 	
 	public void simula(int pezziDaProdurre) {
-		//this.sim = new Simulazione3();
 		this.sim.caricaLinea(pezziDaProdurre);
 		this.sim.run();
 		this.sim.calcolaPerformance();
@@ -91,10 +73,6 @@ public class Model {
 		return this.sim.getWIP();
 	}
 	
-	/*public int getWipInt() {
-		return (int) this.sim.getWipInt();
-	}*/
-	
 	public double getCT() {
 		return this.sim.getCT();
 	}
@@ -106,28 +84,5 @@ public class Model {
 	public double getTempoImpiegatoG() {
 		return this.sim.getTempoImpiegatoG();
 	}
-	
-	/*public ArrayList<Analisi> getBestCase() {
-		return this.sim.getBestCase();
-	}
-	
-	public ArrayList<Analisi> getWorstCase() {
-		return this.sim.getWorstCase();
-	}
-	
-	public ArrayList<Analisi> getPracticalWorstCase() {
-		return this.sim.getPracticalWorstCase();
-	}*/
-	
-	/*public ArrayList<Produzione> lotSizing(HashMap<Integer, Integer> ordiniMese, int capacitaOrd, int capacitaStraord,
-			double costoProdOrd, double costoProdStraord, double costoProdEsterna, double costoStoccaggio) {
-		HashMap<Integer, Produzione> schedOttimaMap = new HashMap<Integer, Produzione>();
-		schedOttimaMap = this.lotSizing.lotSizing(ordiniMese, capacitaOrd, capacitaStraord, 
-				costoProdOrd, costoProdStraord, costoProdEsterna, costoStoccaggio);
-		ArrayList<Produzione> schedOttima = new ArrayList<Produzione>();
-		for(int mese : schedOttimaMap.keySet()) {
-			schedOttima.add(schedOttimaMap.get(mese));
-		}
-		return schedOttima;
-	}*/
+
 }
